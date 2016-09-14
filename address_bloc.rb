749d3da7a0c0051da9fa62c98c8c98e07883ae97
David Kim <dkdavekim@gmail.com>
2016-09-14 07:03:24 +0000

    Checkpoint 20 AddressBloc Menu: Completed

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,12 @@
-puts "Welcome to AddressBloc!"
+require_relative 'controllers/menu_controller'
+
+# #4 
+menu = MenuController.new
+
+# #5
+system "clear"
+puts "Welcome to Address Bloc!"
+
+# #6
+menu.main_menu
+
