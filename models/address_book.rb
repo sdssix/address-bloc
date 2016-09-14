fbe14833422fcc088da9ff8016a8fdc60446e24f
David Kim <dkdavekim@gmail.com>
2016-09-14 23:11:32 +0000

    Assignment 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -63,4 +63,41 @@ class AddressBook
     
     end
     
+     def iterative_search(name)
+        # #1
+        lower = 0
+        upper = entries.length - 1
+        
+        # #2
+        while lower <= upper
+            # #3
+            mid = (lower + upper) / 2
+            mid_name = entries[mid].name
+            
+            # #4
+            if name == mid_name
+                return entries[mid]
+            elsif name < mid_name
+                upper = mid - 1
+            elsif name > mid_name
+                lower = mid + 1
+            end
+        end
+        
+        # #5
+        return nil
+        
+    
+    end
+    
+    def iterative_search(name)
+        @entries.each do |entry|
+            if entry.name == name
+                return entry
+            end
+        end
+        
+        return nil
+    end
+    
 end
